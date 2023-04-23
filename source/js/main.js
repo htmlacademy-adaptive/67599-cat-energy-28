94e3c59667a4a7c78b01e066dc4d2852639df3c6
pavalex <Alex.P79@mail.ru>
2023-04-23 20:19:46 +0800

    Исправил замечания

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,15 @@
-const buttonMenu = document.querySelector('.main-nav__toggle');
+const buttonMenuClose = document.querySelector('.main-nav__close');
 const menu = document.querySelector('.main-nav__list');
+
+// Если JS подключен меню закрывается
+buttonMenuClose.classList.replace('main-nav__close', 'main-nav__open');
+menu.classList.remove('main-nav__list--open');
+
+const buttonMenu = document.querySelector('.main-nav__open');
 
 // Переключение мобильного меню
 buttonMenu.addEventListener('click', function() {
   menu.classList.toggle('main-nav__list--open');
-  buttonMenu.classList.toggle('main-nav__toggle');
-  buttonMenu.classList.toggle('main-nav__toggle--close');
+  buttonMenu.classList.toggle('main-nav__open');
+  buttonMenu.classList.toggle('main-nav__close');
 });
