f7c08725f734e0febaf0e13c1c2827e708a0e6dd
Alex <Alex.P79@mail.ru>
2023-05-02 23:12:45 +0800

    Доделал меню, делал фавикон

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,11 @@
 const buttonMenuClose = document.querySelector('.main-nav__close');
-const menu = document.querySelector('.main-nav__list');
+// const menu = document.querySelector('.main-nav__list');
+const menu = document.querySelector('.site-list');
 
 // Если JS подключен меню закрывается
+menu.classList.add('main-nav__list');
+menu.classList.add('main-nav__list--close');
 buttonMenuClose.classList.replace('main-nav__close', 'main-nav__open');
-menu.classList.remove('main-nav__list--open');
 
 const buttonMenu = document.querySelector('.main-nav__open');
 
