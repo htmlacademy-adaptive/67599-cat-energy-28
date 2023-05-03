2faa2a392cd5c5d3006481487521a2dfea1d4ae8
Alex <Alex.P79@mail.ru>
2023-05-03 23:06:12 +0800

    Доделал правки

--- source/js/main.js
+++ source/js/main.js
@@ -3,8 +3,17 @@ const buttonMenuClose = document.querySelector('.main-nav__close');
 const menu = document.querySelector('.site-list');
 
 // Если JS подключен меню закрывается
-menu.classList.add('main-nav__list');
-menu.classList.add('main-nav__list--close');
+if (menu.classList.contains('main-nav__list')) {
+  menu.classList.remove('main-nav__list');
+} else {
+  menu.classList.add('main-nav__list');
+}
+
+if (menu.classList.contains('main-nav__list--close')) {
+  menu.classList.remove('main-nav__list--close');
+} else {
+  menu.classList.add('main-nav__list--close');
+}
 buttonMenuClose.classList.replace('main-nav__close', 'main-nav__open');
 
 const buttonMenu = document.querySelector('.main-nav__open');
