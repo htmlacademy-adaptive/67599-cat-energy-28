888fb6667a1d09245d3f9e80be47607de4c8be6b
Keks <keks@htmlacademy.ru>
2023-05-10 20:19:03 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,26 @@
+const buttonMenuClose = document.querySelector('.main-nav__close');
+// const menu = document.querySelector('.main-nav__list');
+const menu = document.querySelector('.site-list');
+
+// Если JS подключен меню закрывается
+if (menu.classList.contains('main-nav__list')) {
+  menu.classList.remove('main-nav__list');
+} else {
+  menu.classList.add('main-nav__list');
+}
+
+if (menu.classList.contains('main-nav__list--close')) {
+  menu.classList.remove('main-nav__list--close');
+} else {
+  menu.classList.add('main-nav__list--close');
+}
+buttonMenuClose.classList.replace('main-nav__close', 'main-nav__open');
+
+const buttonMenu = document.querySelector('.main-nav__open');
+
+// Переключение мобильного меню
+buttonMenu.addEventListener('click', function() {
+  menu.classList.toggle('main-nav__list--open');
+  buttonMenu.classList.toggle('main-nav__open');
+  buttonMenu.classList.toggle('main-nav__close');
+});
